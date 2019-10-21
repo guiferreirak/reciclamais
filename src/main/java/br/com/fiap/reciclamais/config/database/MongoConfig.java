3cf4257fff21e7959d48cfe0576020fe16e6fa43
guiferreirak <gui.ferreirak@gmail.com>
2019-10-20 21:55:36 -0300

    Comentando configs mongodb

--- src/main/java/br/com/fiap/reciclamais/config/database/MongoConfig.java
+++ src/main/java/br/com/fiap/reciclamais/config/database/MongoConfig.java
@@ -1,22 +1,22 @@
-package br.com.fiap.reciclamais.config.database;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MongoConfig {
-
-    private static final String URI = "mongodb+srv://developer:developer@reciclamais-npy5o.mongodb.net/test?retryWrites=true&w=majority";
-
-    @Bean
-    public MongoClient connectionFactory(){
-        return new MongoClient(getUriConnection());
-    }
-
-    private MongoClientURI getUriConnection() {
-        return new MongoClientURI(URI);
-    }
-
-}
+//package br.com.fiap.reciclamais.config.database;
+//
+//import com.mongodb.MongoClient;
+//import com.mongodb.MongoClientURI;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class MongoConfig {
+//
+//    private static final String URI = "mongodb+srv://developer:developer@reciclamais-npy5o.mongodb.net/test?retryWrites=true&w=majority";
+//
+//    @Bean
+//    public MongoClient connectionFactory(){
+//        return new MongoClient(getUriConnection());
+//    }
+//
+//    private MongoClientURI getUriConnection() {
+//        return new MongoClientURI(URI);
+//    }
+//
+//}
